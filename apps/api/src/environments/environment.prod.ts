89aa477e14c32cdb6ea87fbfb884491d7632b17d
jason zheng <tso1158687@gmail.com>
2021-09-26 14:23:04 +0800

    add firebase config

--- apps/api/src/environments/environment.prod.ts
+++ apps/api/src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
   production: true,
+  firebase:{
+    "type": "service_account",
+    "project_id": "hello-ironman-2021",
+    "private_key_id": "b1adf4baeefc96d9b0725bd1e6b8ef95cf76abc5",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC7GkaCcMImQSUs\n83VZ9p/Zl4DTmiPdrKsohTE7NASDSe2Lv/P0MNwKXpweKo1dIW6ttHS0pjIwslhE\n9CoD8RgBn0ihLAfPf+L63/DyI8Fju+M8jct//fCHmTqz7OUUTlTbTkgfYYt4L56P\n0hME+37uffSh86ni9Ja3wWaKPwe1MqMXxIQ7NGKG9ZnHOEON57/LPlIOlTawPwOC\n2DG5N1vL0durhl2QN6miLksNLuntV7yOdJVRixQ61y7b9ZwRMbNg1CUtZOkVXLPB\n3TsEBjpuNCXnomoTa2E/U8vQuHQVloetJnVEMR/kOtaoZDQi6uaSbbJpLFcdR+d+\nLrdTkoSzAgMBAAECggEAAQGR/8pgC7k5lftsWb7wOskDdSsjRedQ42gUgE/GcgGM\nPzlOStEZCKTPYoVR4dFL4ig1KLq1H20Icd/4aOO+AGTwArRgSs6+ZOnuuiCnJ2PS\nLT+gMDLELk8eLDwC1kqY9IOoSAqvE4qzbM7mAe6CIeN2HZ2AXNpM3Aw0cRB7fXi9\ne1A4dxx1xwccvGcKzgnB9zEBzeiZ+tH/zSCYrQ3gIxFAx9rhz9n94HOAm2R0qypC\nsUTB2KceHwcKeqeHOCfaidBMTz+/8+N7avd50ADQaNrmSy+mJQ9V/++hUUTFBjfp\nj4yFwSQlbYiaLS1/DNVYlQhfFq9PLeoELIJitA6KgQKBgQDeJxmI5Hr5VCeAdwSF\np8e5oACmhEfRzadmj1j031b8iPwxQV/PmPAKtNHvpYA6KQAkbKo+Stw9h+ke2Ogh\nFYEaW42A2mZ6eqeGf8uRqqwgSJPTABbeZRYgaPlNh3Udw8Gfu05z9Eyawf4XVuG1\nvi0d1u8s0oHKT0x3fk9LUanNMwKBgQDXnBK2QqyaDBRh8zEBi6vIYE1rQ9h3pKvF\nfrwFXTmwBpvc/CVqN30Ak8fSiH0cj6/MjBL0KIPucZezhjhSsrKJS69W68QUqjOr\nMq2ZhMtXhrQ8DZg88xWtnUlPVfgDT9kY58NjdPuddWyUfgW8Y8Fy8PCdP5RD+Ttg\nF7VSO8lqgQKBgEtTmrbuMjyEqnhG4wOm5nQsqsvLosajIxYFmTY+3LH4iPryndg4\nlXwr39uQlLAj4uZ26isBGzjJ8uQAzIUbU5FqZcrBQ2pYWKGkh5Q2+PJhaa5sI1T5\nlc0bix0cxzt2exr8Whe7SJZZHR0w2ovTlly92o+qOVQuG1GRRLXXVOo9AoGBALs5\ndvI3sYPPS3/G2t45t8C3B2NGtdkm8FX9+C08R9S32G6rsZ2rc2ul6BXEmcfMad7Y\nH7lO2Umb8BxfR9df/zLDuw1ii5FaPqXByweNTbnCag3fzDvY5hEtjLRMtaHCm4VJ\n89wQgFG53sCPMlHl2d0Cph4WUJw59zo5PCWdbwiBAoGASp9gfAqJFZ7HC8yyJTfU\ntlsmKdQmP2l7x+60RM0aTgreJuzSyXxs7X9jJN1yMREwzKHJnvr574dL6qo90yEA\nrDsah+VDiAJE9Q5m4qLSkcmr3c9E8qpWytYxk4CQwxy573TwhmWhcVrRmNc2f6t7\nGgWZwHuXtiQiSuvIgsmjtJQ=\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-nv9qd@hello-ironman-2021.iam.gserviceaccount.com",
+    "client_id": "110748648174956240447",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-nv9qd%40hello-ironman-2021.iam.gserviceaccount.com"
+  }
+  
 };
